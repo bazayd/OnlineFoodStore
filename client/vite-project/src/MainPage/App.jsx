c8713af52b855d7fce0ff6fc1c6bc594f596b53a
bazayd <brandonzarriaga@gmail.com>
2024-03-22 19:19:57 -0700

    Added some css effects to navbar options, fixed margin issues.

--- client/vite-project/src/MainPage/App.jsx
+++ client/vite-project/src/MainPage/App.jsx
@@ -12,20 +12,26 @@ const MainPage = () => {
       <ul>
         <li>
           <div className='icon-text'>
-            <img src={Location_Icon} className='Location_Icon'/>
-            <span className='text'>Location</span>
+            <a href="#">
+              <img src={Location_Icon} className='Location_Icon'/>
+              <span className='text'>Location</span>
+            </a>
           </div>
         </li>
         <li>
           <div className='icon-text'>
+            <a href="#">
             <img src={Profile_Icon} className='Profile_Icon'/>
             <span className='text'>Account</span>
+            </a>
           </div>
         </li>
         <li>
           <div className='icon-text'>
-            <img src={Cart_Icon} className='Cart_Icon'/>
-            <span className='text'>Cart</span>
+            <a href="#">
+              <img src={Cart_Icon} className='Cart_Icon'/>
+              <span className='text'>Cart</span>
+            </a>
           </div>
         </li>
       </ul>
